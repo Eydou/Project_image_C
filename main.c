32626a899813b6a8b10c4d63e7f8f2f0b4e14a01
tonio <chouchebel@gmail.com>
2021-03-11 21:41:00 +0100

    add

--- main.c
+++ main.c
@@ -89,6 +89,6 @@ int main(int argc, char**argv)
       fclose(fichier);
   }
   else
-    printf("impossible d'ouvrir le fichier\n");
+    printf("impo ssible d'ouvrir le fichier\n");
   return 0;
 }
